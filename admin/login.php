c2b26c5eb0e7dee3a9d3d9a8aa83c9934f21e8e8
Khant Phone <khantphone333@gmail.com>
2020-11-15 00:40:44 +0630

    password hash

--- admin/login.php
+++ admin/login.php
@@ -12,7 +12,7 @@
 		$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 		if ($user) {
-			if ($user['password'] == $password) {
+			if (password_verify($password,$user['password'])) {
 				$_SESSION['user_id'] = $user['id'];
 				$_SESSION['logged_in'] = time();
 				$_SESSION['user_name'] = $user['name'];
--- admin/userAdd.php
+++ admin/userAdd.php
@@ -26,7 +26,7 @@
       else{
       $name = $_POST['name'];
       $email = $_POST['email'];
-      $password = $_POST['password'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
       
       if (empty( $_POST['role'])) {
        $role = 0;
